712c9548f0641315b8519e3bffd5bc8209ec457d
Rohbiar <73039664+Rohbiar@users.noreply.github.com>
2021-06-27 17:31:39 -0400

    Add files via upload

--- APIcalls.py
+++ APIcalls.py
@@ -2,43 +2,67 @@
 import requests
 import json
 
+token = 'c2nffdaad3i8g7sr4n3g'
+
+
+def getName(ticker):
+    r = requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=' + ticker + '&token=c2nffdaad3i8g7sr4n3g')
+    return r.json()
+
+
+def getTicker(name):
+    r = requests.get('https://finnhub.io/api/v1/search?q=' + name + '&token=c2nffdaad3i8g7sr4n3g')
+
+    return r.json()
+
 
 def socialImpact(ticker):
     r = requests.get('https://finnhub.io/api/v1/stock/social-sentiment?symbol=' + ticker
                      + '&token=c2nffdaad3i8g7sr4n3g')
-    print(r.json())
+    return (r.json())
 
 
 def insiderTrans(ticker):
     r = requests.get('https://finnhub.io/api/v1/stock/insider-transactions?symbol=' +
                      ticker + '&token=c2nffdaad3i8g7sr4n3g')
-    print(r.json())
+    return (r.json())
 
 
 def techIndicator(ticker):
     r = requests.get('https://finnhub.io/api/v1/indicator?symbol=' + ticker + '&resolution=D&from=1583098857&'
                                                                               'to=1584308457&indicator=sma&timeperiod=3&token=c2nffdaad3i8g7sr4n3g')
-    print(r.json())
+    return (r.json())
 
 
 def candles(ticker):
     r = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=' + ticker +
                      '&resolution=1&from=1615298999&to=1615302599&token=c2nffdaad3i8g7sr4n3g')
-    print(r.json())
+    return (r.json())
 
 
 def quote(ticker):
     r = requests.get('https://finnhub.io/api/v1/quote?symbol=' + ticker + '&token=c2nffdaad3i8g7sr4n3g')
-    print(r.json())
+
+    return r.json()
 
 
 def analyst(ticker):
     data = requests.get('https://finnhub.io/api/v1/stock/recommendation?symbol='
-                 + ticker + '&token=c2nffdaad3i8g7sr4n3g')
-    return data
+                        + ticker + '&token=c2nffdaad3i8g7sr4n3g')
+    return data.json()
 
-#socialImpact('AAPL')
-#insiderTrans('AAPL')
-#techIndicator('AAPL')
-#candles('AAPL')
-#quote('AAPL')
+
+def aggregateIndicator(ticker):
+    r = requests.get(
+        'https://finnhub.io/api/v1/scan/technical-indicator?symbol=' + ticker + '&resolution=D&token=c2nffdaad3i8g7sr4n3g')
+    return r.json()
+
+
+def quoteUpdate(ticker):
+    value_dict = quote(ticker)
+    return ((value_dict['c'] - value_dict['o']) / value_dict['o'])
+
+
+# (close - open) / open
+# value_dict = quote('AAPL')
+# print((value_dict['c'] - value_dict['o']) / value_dict['o'])
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import datetime
 import yfinance as yf
-import openpyxl as xl
+import numpy as np
+import pandas as pd
+import openpyxl
+from openpyxl import load_workbook
 import xlsxwriter
+import xlrd
+from pathlib import Path
 from PythonPackage.APIcalls import analyst
 from PythonPackage.APIcalls import socialImpact
 from PythonPackage.APIcalls import insiderTrans
 from PythonPackage.APIcalls import techIndicator
 from PythonPackage.APIcalls import candles
 from PythonPackage.APIcalls import quote
+from PythonPackage.APIcalls import quoteUpdate
+from PythonPackage.APIcalls import getTicker
+from PythonPackage.APIcalls import aggregateIndicator
 
 
 # function returns current price of stock (DELETE)
@@ -17,29 +25,107 @@ def get_current_price(symbol):
     return todays_data['Close'][0]
 
 
+# Datetime to show the update timing
 def date():
     today = datetime.datetime.now()
-    d = datetime.timedelta(days=30)
+    d = datetime.timedelta(days=0)
     a = today - d
-    a = a.strftime('%Y-%m-%d')
+    a = a.strftime('%m/%d/%Y')
     return a
 
 
-tickerList = ["AAPL", "MSFT", "GOOGL", "ABNB", "AMZN", "SPY"]
+def readWbNames():
+    df = pd.read_excel('~$stock.xlsx', sheet_name=0)
+    columns = 1
+    rows = len(df)
+    nameList = []
 
-workbook = xlsxwriter.Workbook('~$stock.xlsx')
-worksheet = workbook.add_worksheet()
+    for column in range(columns):
+        for row in range(rows):
+            nameList.append(df.loc[row][column])
 
-# for col_num, data in enumerate(listAnalyst(tickerList):
-#    worksheet.write(2, col_num + 1, data)
+    return nameList
 
-# for col_num, data in enumerate(listPrice(tickerList)):
-#    worksheet.write(1, col_num + 1, data)
 
-# for col_num, data in enumerate(tickerList):
-#    worksheet.write(0, col_num + 1, data)
+def readWbTicker():
+    df = pd.read_excel('~$stock.xlsx', sheet_name=0)
+    columns = 1
+    rows = len(df)
+    tickerList = []
 
-# for col_num, data in enumerate(tickerList):
-#    worksheet.write(2, col_num + 1, data)
+    for column in range(columns):
+        column = 1
+        for row in range(rows):
+            tickerList.append(df.loc[row][column])
+    return tickerList
 
-workbook.close()
+
+def dateList(tickerList):
+    dateList = []
+    for x in range(len(tickerList)):
+        dateList.append(date())
+    return dateList
+
+
+def quoteList(tickerList):
+    quoteList = []
+    storage = {}
+    for row in range(len(tickerList)):
+        storage = quote(tickerList[row])
+        quoteList.append(storage['c'])
+
+    return quoteList
+
+
+def analystList(tickerList):
+    analystList = []
+    for rec in range(len(tickerList)):
+        rec_dict = analyst(tickerList[rec])
+        strongbuy = rec_dict[0]['strongBuy']
+        buy = rec_dict[0]['buy']
+        hold = rec_dict[0]['hold']
+        sell = rec_dict[0]['sell']
+        strongsell = rec_dict[0]['strongSell']
+        total = strongbuy + buy + hold + sell + strongsell
+        tr = strongbuy * 1 + buy * 2 + hold * 3 + sell * 4 + strongsell * 5
+        rating = tr / total
+        analystList.append(round(rating, 2))
+    return analystList
+
+
+def aggList(tickerList):
+    aggList = []
+    storage_dict = {}
+    for x in range(len(tickerList)):
+        storage_dict = aggregateIndicator(tickerList[x])
+        aggList.append(storage_dict['technicalAnalysis']['signal'])
+    return aggList
+
+
+nameList = readWbNames()
+tickerList = readWbTicker()
+dates = dateList(tickerList)
+quoteList = quoteList(tickerList)
+recList = analystList(tickerList)
+aggregateList = aggList(tickerList)
+
+
+def writepanda(tickerList, dates, quoteList, recList, aggregateList):
+    data = {
+        'Ticker': tickerList,
+        'Date': dates,
+        'Price': quoteList,
+        'Analyst': recList,
+        'Aggregate': aggregateList
+    }
+
+    row_labels = nameList
+
+    df = pd.DataFrame(data=data, index=row_labels)
+
+    writer = pd.ExcelWriter('~$stock.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+
+writepanda(tickerList, dates, quoteList, recList, aggregateList)
